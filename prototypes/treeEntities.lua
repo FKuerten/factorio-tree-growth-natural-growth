079d27b3b34950d79e185bb06c98ce12dbce2804
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2017-01-04 14:52:11 +0100

    extract entitity utilities

--- prototypes/treeEntities.lua
+++ prototypes/treeEntities.lua
@@ -1,60 +1,11 @@
 require "stdlib/string"
 require "stdlib/log/logger"
+require "util/entities"
+
 local sqrt = math.sqrt
 local round = function(x) return math.floor(x+0.5) end
 
 local logger = Logger.new("tree-growth", "main", true)
-
-local scaleBox = function(oldBox, linScale)
-  local newBox = {}
-  for k,v in ipairs(oldBox) do
-    local n = {}
-    for kk,vv in ipairs(v) do
-      n[kk] = vv * linScale
-    end
-    newBox[k] = n
-  end
-  return newBox
-end
-
-local transformPicture = function(oldPic, scale)
-  local linScale = sqrt(scale)
-  local type = oldPic.type
-  local newPic = table.deepcopy(oldPic)
-  if not type then  
-    newPic.width = round(oldPic.width * linScale)
-    newPic.height = round(oldPic.height * linScale)
-    if oldPic.shift then
-      newPic.shift = { oldPic.shift[1] * linScale, oldPic.shift[2] * linScale}
-    end
-  elseif type == "create-particle" then
-    newPic.offset_deviation = scaleBox(oldPic.offset_deviation, linScale)
-    newPic.initial_height = oldPic.initial_height * linScale
-    newPic.initial_height_deviation = oldPic.initial_height_deviation * linScale
-    newPic.speed_from_center = oldPic.speed_from_center * linScale
-  end
-  return newPic
-end
-
-local transformPictures = function(oldPictures, scale)
-  local pictures = {}
-  for i, oldPic in pairs(oldPictures) do
-    pictures[i] = transformPicture(oldPic, scale)
-  end
-  return pictures
-end
-
-local transformVariations = function(oldVariations, scale)
-  local newVariations = {}
-  for i, oldVariation in ipairs(oldVariations) do
-    local newVariation = {}
-    for k, oldPic in pairs(oldVariation) do
-      newVariation[k] = transformPicture(oldPic, scale)
-    end
-    newVariations[i] = newVariation
-  end
-  return newVariations
-end
 
 local transformTree = function(oldTree)
   logger.log("transforming tree " .. oldTree.name)
---
+++ util/entities.lua
@@ -0,0 +1,53 @@
+local sqrt = math.sqrt
+local round = function(x) return math.floor(x+0.5) end
+
+scaleBox = function(oldBox, linScale)
+  local newBox = {}
+  for k,v in ipairs(oldBox) do
+    local n = {}
+    for kk,vv in ipairs(v) do
+      n[kk] = vv * linScale
+    end
+    newBox[k] = n
+  end
+  return newBox
+end
+
+transformPicture = function(oldPic, scale)
+  local linScale = sqrt(scale)
+  local type = oldPic.type
+  local newPic = table.deepcopy(oldPic)
+  if not type then  
+    newPic.width = round(oldPic.width * linScale)
+    newPic.height = round(oldPic.height * linScale)
+    if oldPic.shift then
+      newPic.shift = { oldPic.shift[1] * linScale, oldPic.shift[2] * linScale}
+    end
+  elseif type == "create-particle" then
+    newPic.offset_deviation = scaleBox(oldPic.offset_deviation, linScale)
+    newPic.initial_height = oldPic.initial_height * linScale
+    newPic.initial_height_deviation = oldPic.initial_height_deviation * linScale
+    newPic.speed_from_center = oldPic.speed_from_center * linScale
+  end
+  return newPic
+end
+
+transformPictures = function(oldPictures, scale)
+  local pictures = {}
+  for i, oldPic in pairs(oldPictures) do
+    pictures[i] = transformPicture(oldPic, scale)
+  end
+  return pictures
+end
+
+transformVariations = function(oldVariations, scale)
+  local newVariations = {}
+  for i, oldVariation in ipairs(oldVariations) do
+    local newVariation = {}
+    for k, oldPic in pairs(oldVariation) do
+      newVariation[k] = transformPicture(oldPic, scale)
+    end
+    newVariations[i] = newVariation
+  end
+  return newVariations
+end
