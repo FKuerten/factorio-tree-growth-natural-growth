3387dc543d1b32f4c97d2a29f426c0d17ce6fb95
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2017-01-04 16:27:33 +0100

    recipe mainly for debugging

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,18 @@
 require "prototypes/treeItems"
 require "prototypes/treeEntities"
 
+local createRecipeForSapling = function(saplingItem)
+  local recipe = {
+    type = "recipe",
+    name = saplingItem.name,
+    ingredients = {{"raw-wood",1}},
+    result = saplingItem.name,
+    result_count = 1
+  }
+  data:extend({recipe})
+  return recipe
+end
+
 local oldTrees = data.raw.tree
 for _, oldTree in pairs(oldTrees) do
   local skip = false
@@ -11,6 +23,7 @@ for _, oldTree in pairs(oldTrees) do
 
   if not skip then
     createSaplingEntityFromTree(oldTree)
-    createSaplingItemFromTree(oldTree)
+    local saplingItem = createSaplingItemFromTree(oldTree)
+    createRecipeForSapling(saplingItem)
   end
 end
